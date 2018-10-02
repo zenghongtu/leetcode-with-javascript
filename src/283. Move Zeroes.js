968cc00b9238b64f280af8049179a2bb0dee0a69
zenghongtu <zenghongtu@qq.com>
2018-10-02 11:57:50 +0800

    feat: add 283 annotation

--- src/283. Move Zeroes.js
+++ src/283. Move Zeroes.js
@@ -1,6 +1,6 @@
 /**
  * Created by zenghongtu on 01/10/2018
- * Desc: 283. Move Zeroes
+ * Desc: https://leetcode.com/problems/move-zeroes/description/
  */
 
 /**
@@ -8,14 +8,18 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
+    // 设置数组新的 index
     let cur = 0;
     for (let i = 0; i < nums.length; i++) {
+        // 如果非0,则进行处理
         if (nums[i]) {
+            // 如果 i 不等于 cur,交换它们所在 index 的值
             if (i !== cur) {
                 const tmp = nums[i];
                 nums[i] = nums[cur];
                 nums[cur] = tmp;
             }
+            // cur 始终自增
             cur++
         }
     }
