10edddc775806121af39894b25d9b8457021b38e
zenghongtu <zenghongtu@qq.com>
2018-10-02 11:04:11 +0800

    refactor: optimize 283

--- src/283. Move Zeroes.js
+++ src/283. Move Zeroes.js
@@ -8,21 +8,22 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
-    const tmpList = [];
-    for (let i = 0, len = nums.length; i < len; i++) {
-        nums[i] && tmpList.push(nums[i])
-    }
-    for (let i = 0, len = tmpList.length; i < len; i++) {
-        nums[i] = tmpList[i]
-    }
-    for (let i = tmpList.length, len = nums.length; i < len; i++) {
-        nums[i] = 0
+    let cur = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i]) {
+            if (i !== cur) {
+                const tmp = nums[i];
+                nums[i] = nums[cur];
+                nums[cur] = tmp;
+            }
+            cur++
+        }
     }
 };
 
-const nums = [0, 1, 0, 3, 12];
+const nums = [0, 1];
 moveZeroes(nums);
-if (JSON.stringify(nums) === '[1,3,12,0,0]') {
+if (JSON.stringify(nums) === '[1,0]') {
     console.log('测试通过')
 } else {
     console.log('测试失败');
