baac36dbdc386360b3674c77623dea2934538c83
zenghongtu <zenghongtu@qq.com>
2018-10-04 14:42:14 +0800

    feat: add 209

--- src/209. Minimum Size Subarray Sum.js
+++ src/209. Minimum Size Subarray Sum.js
@@ -9,20 +9,41 @@
  * @param {number[]} nums
  * @return {number}
  */
+// 思路:滑动窗口
 var minSubArrayLen = function (s, nums) {
-    let i = 0, len = nums.length, subArrayLen = len;
-    while (i < len) {
-        let tmp = 0;
-        let j = i + 1;
-        while (nums[i] + tmp < s) {
-            tmp += nums[j++]
+    // 初始化左边为0
+    let l = 0,
+        // 初始化右边为-1
+        r = -1,
+        // 初始化 len 为 nums 长度
+        len = nums.length,
+        // 初始化最小子数组长度为 len+1
+        minLen = len + 1,
+        // 初始化当前子数组值为0
+        sum = 0;
+    // 如果左边 index 小于 nums 长度,则进行处理
+    while (l < len) {
+        // 左边index + 1 小于 nums 长度并且 sum 小于目标值 则进行处理
+        if (r + 1 < len && sum < s) {
+            // 累加右侧值
+            sum += nums[++r]
+        } else {
+            // 减去左侧当前值
+            sum -= nums[l++]
         }
-        if ((nums[i] + tmp) === s) {
-            subArrayLen = Math.min(j - i, subArrayLen)
+        // 如果 sum 大于目标值
+        if (sum >= s) {
+            // 取当前子数组和新子数组中长度的最小值
+            minLen = Math.min(minLen, r - l + 1)
         }
-        i++
     }
-    return subArrayLen
+    // 如果子数组长度大于数组长度
+    if (minLen > len) {
+        // 返回0
+        return 0
+    }
+    // 返回最小子数组长度
+    return minLen
 };
 
 
