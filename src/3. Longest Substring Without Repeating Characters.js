1279326759d0aaebbe3059b6ace02771bfa27288
zenghongtu <zenghongtu@qq.com>
2018-10-04 16:29:24 +0800

    feat: add 3

--- src/3. Longest Substring Without Repeating Characters.js
+++ src/3. Longest Substring Without Repeating Characters.js
@@ -7,23 +7,31 @@
  * @param {string} s
  * @return {number}
  */
+//  滑动窗口
 var lengthOfLongestSubstring = function (s) {
+    // 左侧 index
     let l = 0,
+        // 右侧 index
         r = -1,
+        // s 长度
         len = s.length,
+        // 最大子数组长度
         maxLen = 0,
-        subString = '';
+        // 记录 s 中字母频率
+        freq = {};
     while (l < len) {
-        if (r + 1 < len && !subString.includes(s[++r])) {
-            subString += s[r]
+        // 如果 r 不位于最末尾 并且 当前 index 对应字母频率未定义(意指0)
+        if (r + 1 < len && !freq[s[r + 1]]) {
+            // 右侧 index 往右移一位,并记录所在字母频率为1
+            freq[s[++r]] = 1
         } else {
-            maxLen = Math.max(maxLen, subString.length);
-            const i = subString.indexOf(s[r]);
-            subString = subString.slice(i + 1);
-            subString += s[r];
-            l++
+            // 左侧 index 所在字母频率减一(为0),并往右移一位
+            freq[s[l++]]--
         }
+        // 设置 maxLen 为当前值与当前 r 和 l (闭区间) 中最大值
+        maxLen = Math.max(maxLen, r - l + 1);
     }
+    // 返回 maxLen, 即子串最大长度
     return maxLen
 };
 
