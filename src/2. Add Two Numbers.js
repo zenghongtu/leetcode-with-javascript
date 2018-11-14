98574cf92b48f445fd5a731fcbe30e3633acf44d
zenghongtu <zenghongtu@gmail.com>
2018-11-14 20:47:58 +0800

    feat: add 2

--- src/2. Add Two Numbers.js
+++ src/2. Add Two Numbers.js
@@ -15,30 +15,45 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function (l1, l2) {
-  let head, node, node1 = l1, node2 = l2, x = 0;
-  while (node1 || node2) {
-    let val1 = (node1 && node1.val) || 0;
-    let val2 = (node2 && node2.val) || 0;
-    let num = val1 + val2;
-    if (node) {
-      node.next = new ListNode((num >= 10 ? num - 10 : num) + x);
-      node = node.next
-    } else {
-      head = node = new ListNode((num >= 10 ? num - 10 : num) + x)
-    }
-    num >= 10 ? x = 1 : x = 0;
 
-    node1 = (node1 && node1.next) || null;
-    node2 = (node2 && node2.next) || null
+var addTwoNumbers = function (l1, l2) {
+  // l1 对应当前节点
+  let node1 = l1,
+    // l2 对应当前节点
+    node2 = l2;
+  // 虚拟头节点
+  let dummy = new ListNode(null);
+  // 当前节点
+  let cur = dummy;
+  // 进一位
+  let carry = 0;
+  // 当某一条链节点不为空
+  while (node1 !== null || node2 !== null) {
+    // 当前节点值
+    let d1 = node1 === null ? 0 : node1.val;
+    let d2 = node2 === null ? 0 : node2.val;
+    // 求和
+    let sum = d1 + d2 + carry;
+    // 如果大于等于10,进一位
+    carry = sum >= 10 ? 1 : 0;
+    // 节点下一个值
+    cur.next = new ListNode(sum % 10);
+    cur = cur.next;
+    // 遍历节点
+    if (node1 !== null) node1 = node1.next;
+    if (node2 !== null) node2 = node2.next
   }
-  return head
+  // 如果是1说明还需要进一位,生成一个节点
+  if (carry === 1) cur.next = new ListNode(carry);
+  // 返回虚拟节点的下一个点
+  return dummy.next
 };
 
-const l1 = [0],
-  l2 = [7, 3];
+
+const l1 = [2, 4, 3],
+  l2 = [5, 6, 4];
 const result = addTwoNumbers(createList(l1), createList(l2));
-if (JSON.stringify(result) === JSON.stringify(createList([7, 3]))) {
+if (JSON.stringify(result) === JSON.stringify(createList([7, 0, 8]))) {
   console.log('测试通过');
 } else {
   console.log(result);
