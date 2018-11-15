9dc59c82687283dec36f763ec94aa373d952d3d6
zenghongtu <zenghongtu@gmail.com>
2018-11-15 20:27:49 +0800

    optimize 455

--- src/445. Add Two Numbers II.js
+++ src/445. Add Two Numbers II.js
@@ -16,52 +16,83 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
+// var addTwoNumbers = function (l1, l2) {
+//   // 分别存储两个链表转换成的数组
+//   let arr1 = [],
+//     arr2 = [];
+//   // 分别存储两个链表的当前节点
+//   let node1 = l1,
+//     node2 = l2;
+//   // 返回链表的头节点
+//   let dummy = null;
+//   // 遍历两个链表,存入数组中
+//   while (node1 || node2) {
+//     if (node1 && node1.val !== undefined) {
+//       arr1.push(node1.val);
+//       node1 = node1.next
+//     }
+//     if (node2 && node2.val !== undefined) {
+//       arr2.push(node2.val);
+//       node2 = node2.next
+//     }
+//   }
+//   let len1 = arr1.length,
+//     len2 = arr2.length,
+//     i = 1,
+//     // 记录上一个值是否大于等于10
+//     carry = 0;
+//   // 从末尾遍历两个数组
+//   while (arr1[len1 - i] !== undefined || arr2[len2 - i] !== undefined) {
+//     // 求和
+//     let sum = (arr1[len1 - i] || 0) + (arr2[len2 - i] || 0) + carry;
+//     // 如果大于等于10, carry 记录一下
+//     carry = sum >= 10 ? 1 : 0;
+//     // 根据值生成当前节点
+//     let node = new ListNode(sum % 10);
+//     // 当前节点指向前一节点
+//     node.next = dummy;
+//     // 把当前节点设为头节点
+//     dummy = node;
+//     i++
+//   }
+//   // 如果最后carry为1,说明还有剩一位,继续生成头节点
+//   if (carry === 1) {
+//     let node = new ListNode(1);
+//     node.next = dummy;
+//     dummy = node
+//   }
+//   // 返回头节点
+//   return dummy
+// };
+
+  // 用栈来代替数组, 优化逻辑
 var addTwoNumbers = function (l1, l2) {
-  // 分别存储两个链表转换成的数组
-  let arr1 = [],
-    arr2 = [];
-  // 分别存储两个链表的当前节点
-  let node1 = l1,
-    node2 = l2;
-  // 返回链表的头节点
-  let dummy = null;
-  // 遍历两个链表,存入数组中
-  while (node1 || node2) {
-    if (node1 && node1.val !== undefined) {
-      arr1.push(node1.val);
+    let node1 = l1, node2 = l2, stack1 = [], stack2 = [], dummy = null, carry = 0;
+    while (node1) {
+      // 入栈
+      stack1.push(node1.val);
       node1 = node1.next
     }
-    if (node2 && node2.val !== undefined) {
-      arr2.push(node2.val);
+    while (node2) {
+      stack2.push(node2.val);
       node2 = node2.next
     }
-  }
-  let len1 = arr1.length,
-    len2 = arr2.length,
-    i = 1,
-    // 记录上一个值是否大于等于10
-    carry = 0;
-  // 从末尾遍历两个数组
-  while (arr1[len1 - i] !== undefined || arr2[len2 - i] !== undefined) {
-    // 求和
-    let sum = (arr1[len1 - i] || 0) + (arr2[len2 - i] || 0) + carry;
-    // 如果大于等于10, carry 记录一下
-    carry = sum >= 10 ? 1 : 0;
-    // 根据值生成当前节点
-    let node = new ListNode(sum % 10);
-    // 当前节点指向前一节点
+
+    while (stack1.length > 0 || stack2.length > 0) {
+      // 出栈
+      const sum = (stack1.pop() || 0)
+        + (stack2.pop() || 0)
+        + carry;
+      carry = sum > 9 ? 1 : 0;
+      const node = new ListNode(sum % 10);
     node.next = dummy;
-    // 把当前节点设为头节点
-    dummy = node;
-    i++
+      dummy = node
   }
-  // 如果最后carry为1,说明还有剩一位,继续生成头节点
   if (carry === 1) {
-    let node = new ListNode(1);
+    const node = new ListNode(1);
     node.next = dummy;
     dummy = node
   }
-  // 返回头节点
   return dummy
 };
 
