cb17d11d33781b34c2cb5fb6127fd6b22b32b551
jasonzeng <jason.zeng@ximalaya.com>
2019-07-25 10:32:23 +0800

    add 235

---
+++ src/235.lowest-common-ancestor-of-a-binary-search-tree.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=235 lang=javascript
+ *
+ * [235] Lowest Common Ancestor of a Binary Search Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+  let curr = root;
+  while (true) {
+    if (p.val < curr.val && q.val < curr.val) {
+      curr = curr.left;
+    } else if (p.val > curr.val && q.val > curr.val) {
+      curr = curr.right;
+    } else {
+      return curr;
+    }
+  }
+};
