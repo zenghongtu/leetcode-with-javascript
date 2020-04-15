f2d5c8bd6d4069c8c739da1473ea21660128e351
zenghongtu <zenghongtu@gmail.com>
2020-04-15 22:17:11 +0800

    rm

--- src/143. Reorder List.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by zenghongtu on 22/11/2018.
- * Desc: https://leetcode.com/problems/reorder-list/
- */
--- src/148. Sort List.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by zenghongtu on 21/11/2018.
- * Desc: https://leetcode.com/problems/sort-list/
- */
