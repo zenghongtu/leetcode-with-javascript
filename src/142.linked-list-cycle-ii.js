f02f602c25d0038ec713ceca3d917039abfa632b
zenghongtu <zenghongtu@gmail.com>
2020-04-16 10:12:44 +0800

    update 142

--- src/142.linked-list-cycle-ii.js
+++ src/142.linked-list-cycle-ii.js
@@ -17,7 +17,7 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var detectCycle = function (head) {
+var detectCycle0 = function (head) {
   const nodeMap = new Map();
   let node = head;
 
@@ -31,4 +31,42 @@ var detectCycle = function (head) {
   }
   return null;
 };
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+  // 快慢指针
+  let s = head,
+    f = head;
+
+  while (f && f.next) {
+    s = s.next;
+    f = f.next.next;
+    // 得出是环，但不一定是入口节点
+    if (s === f) {
+      // 将慢指针重置
+      s = head;
+
+      // 找到相交节点（这个节点为入口点）
+      while (s !== f) {
+        s = s.next;
+        f = f.next;
+      }
+      return s;
+    }
+  }
+
+  return null;
+};
+
 // @lc code=end
+
+/*
+解题方案：
+1. Map
+2. Set
+3. 快慢指针，判断是环后重置找入口节点 [](https://leetcode.com/problems/linked-list-cycle-ii/discuss/495311/JavaScript-Two-Pointers-w-Extended-Notes)
+4. 快慢指针两次（一次判断是环，二次寻找入口节点）
+*/
