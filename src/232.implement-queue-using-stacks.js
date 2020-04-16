2d9c65f567b98fa43e593092f3a90abad6e05423
zenghongtu <zenghongtu@gmail.com>
2020-04-16 23:12:13 +0800

    add 232

---
+++ src/232.implement-queue-using-stacks.js
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=232 lang=javascript
+ *
+ * [232] Implement Queue using Stacks
+ */
+
+// @lc code=start
+/**
+ * Initialize your data structure here.
+ */
+var MyQueue = function () {
+  this._arr = [];
+};
+
+/**
+ * Push element x to the back of queue.
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+  this._arr.push(x);
+};
+
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+  return this._arr.shift();
+};
+
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+  return this._arr[0];
+};
+
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+  return this._arr.length === 0;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
+// @lc code=end
