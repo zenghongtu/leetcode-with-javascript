5c795b00e95dddd8aac21f8dfbf1f7184276109c
zenghongtu <zenghongtu@gmail.com>
2020-04-19 17:17:30 +0800

    add 94

--- src/94.binary-tree-inorder-traversal.js
+++ src/94.binary-tree-inorder-traversal.js
@@ -16,13 +16,14 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var inorderTraversal = function (root) {
+var inorderTraversal1 = function (root) {
   if (!root) {
     return [];
   }
 
   const ret = [];
 
+  // 递归左-中-右
   const walker = (node) => {
     node.left && walker(node.left);
     ret.push(node.val);
@@ -30,6 +31,33 @@ var inorderTraversal = function (root) {
   };
 
   walker(root);
+  return ret;
+};
+
+var inorderTraversal = function (root) {
+  if (!root) {
+    return [];
+  }
+
+  const stack = [];
+  const ret = [];
+
+  // 循环
+  while (root || stack.length) {
+    while (root) {
+      // 先将左节点全部加入栈中
+      stack.push(root);
+      root = root.left;
+    }
+
+    // 出栈
+    root = stack.pop();
+    // 取值
+    ret.push(root.val);
+    // 处理右边节点
+    root = root.right;
+  }
+
   return ret;
 };
 // @lc code=end
