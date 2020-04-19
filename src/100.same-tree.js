a8bf9ce050de67a2413c31a8e7fb89d5f0927a4e
zenghongtu <zenghongtu@gmail.com>
2020-04-19 17:31:24 +0800

    add 100

---
+++ src/100.same-tree.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=100 lang=javascript
+ *
+ * [100] Same Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+  if (!p && !q) return true;
+
+  // p 和 q 其中一值不存在，或者 q 的值不等于 p，返回 false
+  if (!p || !q || p.val !== q.val) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+// @lc code=end
